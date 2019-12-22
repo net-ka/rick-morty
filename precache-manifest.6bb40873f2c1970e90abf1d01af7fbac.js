6c0bffad1e95e7dfe94f45d026abc54b0062747f
net-ka <a.k.fedosova@gmail.com>
2019-12-22 03:47:33 +0200

    Updates

---
+++ precache-manifest.6bb40873f2c1970e90abf1d01af7fbac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f95aa3a7ad87e0f7f5c46cfea83a66c",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "9881237e1548232a714a",
+    "url": "/rick-morty/static/css/main.bd803478.chunk.css"
+  },
+  {
+    "revision": "bc8be8cdf9414b4a8a7b",
+    "url": "/rick-morty/static/js/2.7d466ff2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/rick-morty/static/js/2.7d466ff2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9881237e1548232a714a",
+    "url": "/rick-morty/static/js/main.172b3fb7.chunk.js"
+  },
+  {
+    "revision": "86d872ffd89f0ed52da9",
+    "url": "/rick-morty/static/js/runtime-main.e33a6005.js"
+  },
+  {
+    "revision": "87c42d7c9e9074e8576e5b1b271b57a1",
+    "url": "/rick-morty/static/media/Arial-BoldMT.87c42d7c.woff"
+  },
+  {
+    "revision": "e3e25078783f7c68baa17f05a6d4ae75",
+    "url": "/rick-morty/static/media/Arial-BoldMT.e3e25078.otf"
+  },
+  {
+    "revision": "b267362fe0bf162719681f797a756433",
+    "url": "/rick-morty/static/media/ArialMT.b267362f.otf"
+  },
+  {
+    "revision": "cdb2897cc3b8eddde4bfd6a90e2d40d6",
+    "url": "/rick-morty/static/media/ArialMT.cdb2897c.woff"
+  }
+]);
